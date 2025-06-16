53cd09fcdf62fa1c4b87ae33069d8a564ab18e6d
Lessa Apolinario <lessaapolinario@gmail.com>
2025-06-16 18:55:13 -0300

    Add frontend design

--- src/ui/frontend/components/home/FileConverter/index.tsx
+++ src/ui/frontend/components/home/FileConverter/index.tsx
@@ -1,12 +1,42 @@
-import { useRef, useState, type ChangeEvent } from 'react'
-import { FaPlus } from 'react-icons/fa'
-import axios from 'axios'
 import { env } from '@/core/utils/env'
+import axios from 'axios'
+import { useMemo, useRef, useState, type ChangeEvent } from 'react'
+import { FaPlus } from 'react-icons/fa'
+import { IoIosArrowForward } from 'react-icons/io'
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '../../radix/select'
+
+interface FileConverterOption {
+  value: string
+  text: string
+}
+
+type FileConverterHandler = () => Promise<void>
 
 export function FileConverter() {
   const [isConverting, setIsConverting] = useState(false)
+  const [fileType, setFileType] = useState('')
   const [file, setFile] = useState<File | undefined>()
   const downloadFileRef = useRef<HTMLAnchorElement>(null)
+
+  const options = useMemo<FileConverterOption[]>(() => {
+    return [
+      {
+        value: 'docx-to-pdf',
+        text: 'Docx para PDF',
+      },
+      {
+        value: 'csv-to-xls',
+        text: 'CSV para XLS',
+      },
+    ]
+  }, [])
 
   function handleFileChange(event: ChangeEvent<HTMLInputElement>) {
     const files = event.target.files
@@ -15,38 +45,57 @@ export function FileConverter() {
     }
   }
 
+  function updateCurrentFileType(value: string) {
+    setFileType(value)
+  }
+
+  async function convertDocxFileToPdf() {
+    if (!file) {
+      console.log('Sem arquivo')
+      return
+    }
+
+    const formData = new FormData()
+    formData.append('file', file)
+
+    const response = await axios.post(
+      `${env.VITE_FILE_CONVERTER_API_URL}/api/document/docx-to-pdf`,
+      formData,
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+        responseType: 'blob',
+      }
+    )
+
+    if (downloadFileRef.current) {
+      const blob = new Blob([response.data], { type: 'application/pdf' })
+      const url = window.URL.createObjectURL(blob)
+
+      downloadFileRef.current.href = url
+      downloadFileRef.current.download = file.name.replace('.docx', '.pdf')
+      downloadFileRef.current.click()
+
+      window.URL.revokeObjectURL(url)
+    }
+  }
+
+  async function convertCsvFileToXls() {
+    console.log('Sem função ainda')
+  }
+
   async function handleConvertFile() {
     try {
       setIsConverting(true)
 
-      if (!file) {
-        console.log('Sem arquivo')
-        return
+      const availableConverters: Record<string, FileConverterHandler> = {
+        'docx-to-pdf': convertDocxFileToPdf,
+        'csv-to-xls': convertCsvFileToXls,
       }
 
-      const formData = new FormData()
-      formData.append('file', file)
-
-      const response = await axios.post(
-        `${env.VITE_FILE_CONVERTER_API_URL}/api/document/docx-to-pdf`,
-        formData,
-        {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
-          responseType: 'blob',
-        }
-      )
-
-      if (downloadFileRef.current) {
-        const blob = new Blob([response.data], { type: 'application/pdf' })
-        const url = window.URL.createObjectURL(blob)
-
-        downloadFileRef.current.href = url
-        downloadFileRef.current.download = file.name.replace('.docx', '.pdf')
-        downloadFileRef.current.click()
-
-        window.URL.revokeObjectURL(url)
+      if (availableConverters[fileType] && !!fileType.length) {
+        await availableConverters[fileType]()
       }
 
       setIsConverting(false)
@@ -56,30 +105,49 @@ export function FileConverter() {
   }
 
   return (
-    <div className='grid h-20 cursor-pointer grid-cols-3 place-items-center rounded-xl border-2'>
-      <div className='bg-zinc-200 text-center text-zinc-800'>Select</div>
+    <div className='grid h-20 cursor-pointer grid-cols-3 place-items-center'>
+      <div className='text-center'>
+        <Select onValueChange={updateCurrentFileType}>
+          <SelectTrigger className='w-[180px]'>
+            <SelectValue placeholder='Selecione um conversor' />
+          </SelectTrigger>
+          <SelectContent>
+            {options.map((option) => {
+              return (
+                <SelectItem
+                  className='cursor-pointer bg-zinc-700 text-zinc-200'
+                  key={option.value}
+                  value={option.value}
+                >
+                  {option.text}
+                </SelectItem>
+              )
+            })}
+          </SelectContent>
+        </Select>
+      </div>
 
-      <label htmlFor='convertFileInput' className='cursor-pointer px-4'>
-        <span className='flex items-center justify-center gap-2 text-center text-xl'>
-          Converter arquivo
-          <FaPlus className='text-2xl' />
-        </span>
-
+      <label
+        htmlFor='convertFileInput'
+        className='text-bold flex cursor-pointer items-center justify-center gap-3.5 rounded-xl bg-zinc-700 p-4 text-lg font-bold'
+      >
+        Selecionar arquivo <FaPlus />
         <input
           id='convertFileInput'
           type='file'
           className='sr-only'
-          disabled={isConverting}
+          disabled={fileType === 'Selecione' || isConverting}
           onChange={handleFileChange}
         />
       </label>
 
       <button
         type='button'
-        className='text-bold h-full w-full cursor-pointer rounded-tr-xl rounded-br-xl bg-zinc-700 text-lg font-bold'
+        className='text-bold flex cursor-pointer items-center justify-center gap-3.5 rounded-xl bg-zinc-700 p-4 text-lg font-bold'
         onClick={handleConvertFile}
       >
         Converter
+        <IoIosArrowForward />
       </button>
 
       <a ref={downloadFileRef} className='sr-only'></a>
